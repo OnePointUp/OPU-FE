210b9631fb5a6a6c2620e8f48f275f2af2f6dea2
lddocy <jkcy911219@naver.com>
2025-11-09 19:54:25 +0900

    :art: style: 메인 레이아웃 크기 조정

--- opu-fe/src/app/(main)/layout.tsx
+++ opu-fe/src/app/(main)/layout.tsx
@@ -14,10 +14,10 @@ export default function MainLayout({
     const showHeader = !HIDE_HEADER.has(pathname);
 
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-[100svh]">
             {/* 테스트용 헤더 렌더링 : 실제 페이지 만든 후에 title명 변경, show={showHeader}로 바꾸기*/}
-            <Header title="회원가입" show={true} showBack />
-            <main className="flex-1">{children}</main>
+            <Header title="홈" show={true} showBack />
+            <main className="flex-1 pb-[72px]">{children}</main>
             <Menu />
         </div>
     );
