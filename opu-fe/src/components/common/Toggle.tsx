90f261a071c76745b28f266b4541b321dd09c921
lddocy <jkcy911219@naver.com>
2025-11-11 13:17:13 +0900

    :truck:	 move: toggle 컴포넌트 common으로 이동

---
+++ opu-fe/src/components/common/Toggle.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+type Props = {
+    checked: boolean;
+    onChange: (v: boolean) => void;
+    disabled?: boolean;
+    id?: string;
+};
+
+export default function Switch({ checked, onChange, disabled, id }: Props) {
+    return (
+        <button
+            id={id}
+            type="button"
+            role="switch"
+            aria-checked={checked}
+            disabled={disabled}
+            onClick={() => !disabled && onChange(!checked)}
+            className={`relative inline-flex h-[30px] w-13 items-center rounded-full transition
+                ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}
+                ${
+                    checked
+                        ? "bg-[var(--color-toggle-on)]"
+                        : "bg-[var(--color-toggle-off)]"
+                }
+              `}
+        >
+            <span
+                className={`inline-block h-[24px] w-[24px] transform rounded-full bg-white transition
+          ${checked ? "translate-x-6" : "translate-x-1"}
+        `}
+            />
+        </button>
+    );
+}
