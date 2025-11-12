5b12b8ebf8ab5fa07e436be5fa276cac1a1e2a9f
lddocy <jkcy911219@naver.com>
2025-11-13 05:44:38 +0900

    :art: style: 차단한 OPU 목록 -> 차단 OPU 목록으로 명칭 변경

--- opu-fe/src/app/(mypage)/me/page.tsx
+++ opu-fe/src/app/(mypage)/me/page.tsx
@@ -15,7 +15,7 @@ export default function MyPage() {
     const [loading, setLoading] = useState(true);
 
     const items = [
-        { label: "차단한 OPU 관리", href: "/me/blocked-opu" },
+        { label: "차단 OPU 관리", href: "/me/blocked-opu" },
         { label: "알림 설정", href: "/me/notification" },
         { label: "비밀번호 변경", href: "/me/password" },
         { label: "로그아웃", onClick: () => console.log("로그아웃!") },
