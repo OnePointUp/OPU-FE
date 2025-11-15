d8ee1d59413dfd8a982f539b928a260e0d59709d
lddocy <jkcy911219@naver.com>
2025-11-16 04:27:41 +0900

    :recycle: refactor: 차단 opu 목록 페이지 재구조화

--- opu-fe/src/app/(mypage)/me/blocked-opu/page.tsx
+++ opu-fe/src/app/(mypage)/me/blocked-opu/page.tsx
@@ -1,9 +1,24 @@
+import Header from "@/components/layout/Header";
 import BlockedOpuManagePage from "@/features/blocked-opu/pages/BlockedOpuManagePage";
 import { getBlockedOpuList } from "@/features/blocked-opu/services";
-import { mockUser } from "@/mocks/api/handler/user";
+import { CURRENT_MEMBER_ID } from "@/mocks/api/db/member.db";
 
 export default async function Page() {
-    const items = await getBlockedOpuList(mockUser.id);
+    const items = await getBlockedOpuList(CURRENT_MEMBER_ID);
 
-    return <BlockedOpuManagePage initialItems={items} />;
+    return (
+        <div className="app-page">
+            <Header
+                title="차단 OPU 관리"
+                tooltip={{
+                    message: [
+                        "차단을 해제한 OPU는 랜덤 뽑기 시",
+                        "다시 나타날 수 있으니 참고하시기 바랍니다.",
+                    ],
+                    position: "bottom",
+                }}
+            />
+            <BlockedOpuManagePage initialItems={items} />
+        </div>
+    );
 }
