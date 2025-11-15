af65a9ea7e3b0289ba91b7eb4e9f5e91c6aa619f
didiha <hcring@naver.com>
2025-11-16 00:18:19 +0900

    비밀번호 찾기 이메일 입력 화면 구현

---
+++ opu-fe/src/app/(auth)/find-pw/page.tsx
@@ -0,0 +1,4 @@
+import FindPasswordPage from "@/features/auth/pages/FindPasswordPage";
+export default function Page() {
+    return <FindPasswordPage />;
+}
---
+++ opu-fe/src/features/auth/pages/FindPasswordPage.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Header from "@/components/layout/Header";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import EmailField from "../components/EmailField";
+import PasswordInput from "@/features/user/components/PasswordInput";
+import MainButton from "@/components/common/MainButton"
+import { validateEmail } from "../services";
+import { toastError, toastSuccess } from "@/lib/toast";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(""); 
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const isActive =
+    email.trim() &&
+    !emailError;
+
+  async function handleSubmit() {
+    try {
+          
+          toastSuccess("이메일을 전송하였습니다.");
+          router.push("/");
+        } catch (e) {
+          toastError("이메일 전송 중 문제가 생겼습니다. 다시 시도해주세요.");
+        }
+  }
+
+  // 입력할 때마다 자동 검사
+  function handleEmailChange(v: string) {
+    setEmail(v);
+    setEmailError(validateEmail(v));
+  }
+
+  return (
+    <div className="app-page overflow-hidden overscroll-none">
+      <Header title="비밀번호 찾기" showBack />
+
+       <main className="app-container pt-app-header pb-40 px-6 flex flex-col items-center">
+        {/* 설명 */}
+        <div className="mt-[62px]">
+          <p
+            style={{
+              font: "var(--text-sub)",
+              color: "var(--color-dark-gray)",
+              fontWeight: "var(--weight-semibold)"
+            }}
+            className="text-center">
+              비밀번호 재설정을 위해
+              <br />
+              가입하신 이메일 주소를 입력해주세요
+          </p>
+        </div>
+
+        {/* 입력 폼 */}
+        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-[17px]">
+          <EmailField
+                    value={email}
+                    onChange={handleEmailChange}
+                    error={emailError}
+                    isLabeled={false}
+                  />
+
+          {/* 로그인 버튼 */}
+          <MainButton label="확인" onClick={handleSubmit} fullWidth={true} disabled={!isActive}/>
+        </form>
+      </main>
+    </div>
+  );
+}
