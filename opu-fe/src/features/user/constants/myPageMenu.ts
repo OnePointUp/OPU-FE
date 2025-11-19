5cdc5b0f58d4c0768ba8864d3ecec834c5293df9
lddocy <jkcy911219@naver.com>
2025-11-19 23:30:59 +0900

    :recycle:	refactor: 마이페이지 설정 목록 재구성

--- opu-fe/src/features/user/constants/myPageMenu.ts
+++ opu-fe/src/features/user/constants/myPageMenu.ts
@@ -1,6 +1,5 @@
 export const myPageMenuItems = [
-    { label: "찜", href: "/me/liked-opu" },
-    { label: "차단 OPU 관리", href: "/me/blocked-opu" },
+    { label: "차단 OPU 관리", href: "/opu/blocked" },
     { label: "알림 설정", href: "/me/notification" },
     { label: "비밀번호 변경", href: "/me/password" },
     { label: "로그아웃", onClick: () => console.log("로그아웃!") },
