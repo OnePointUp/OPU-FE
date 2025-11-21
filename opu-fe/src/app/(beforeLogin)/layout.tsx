43d47951ed33916f485cf39680aaa2ef33724b7c
lddocy <jkcy911219@naver.com>
2025-11-22 07:34:03 +0900

    :art: style: layout 스크롤 안 되게 수정

--- opu-fe/src/app/(beforeLogin)/layout.tsx
+++ opu-fe/src/app/(beforeLogin)/layout.tsx
@@ -4,8 +4,8 @@ export default function AfterLoginLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="app-page flex flex-col">
-            <main className="app-container pt-app-header pb-40 px-6 mt-8">
+        <div className="app-page flex flex-col h-screen overflow-hidden">
+            <main className="app-container pt-app-header pb-40 px-6 mt-8 overflow-hidden overscroll-none">
                 {children}
             </main>
         </div>
