3bb05c1bcc22711f746f610b05089111aa4c8f8e
lddocy <jkcy911219@naver.com>
2025-12-02 08:25:03 +0900

    :heavy_plus_sign: chore: 에러코드 확인 함수 추가

--- opu-fe/src/utils/api-helpers.ts
+++ opu-fe/src/utils/api-helpers.ts
@@ -1,5 +1,21 @@
 import axios from "axios";
 
+type ApiErrorPayload = {
+    errorCode?: string;
+    message?: string;
+};
+
+type ApiErrorWithResponse = {
+    response?: {
+        data?: ApiErrorPayload;
+    };
+};
+
+export function extractErrorCode(err: unknown): string | undefined {
+    const e = err as ApiErrorWithResponse;
+    return e.response?.data?.errorCode;
+}
+
 export function extractErrorMessage(err: unknown, fallback: string): string {
     if (axios.isAxiosError(err)) {
         return (
