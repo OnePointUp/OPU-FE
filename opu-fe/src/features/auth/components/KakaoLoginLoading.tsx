541c794c5fe4f6748027b934ada2c258abd0f9e7
lddocy <jkcy911219@naver.com>
2025-12-02 17:17:52 +0900

    :art: style: 카카오 로그인 로딩 화면 div 위치 수정

--- opu-fe/src/features/auth/components/KakaoLoginLoading.tsx
+++ opu-fe/src/features/auth/components/KakaoLoginLoading.tsx
@@ -4,26 +4,27 @@ import Image from "next/image";
 
 export default function KakaoLoginLoading() {
     return (
-        <section className="flex min-h-[100svh] flex-col items-center justify-center gap-4">
+        <section className="flex flex-col gap-3 items-center justify-center mt-50">
             {/* 마스코트 / 일러스트 */}
-            <div className="flex flex-col items-center gap-3">
-                <Image
-                    src="/images/cabit_hello.png"
-                    alt="카카오 로그인 중인 OPU 캐릭터"
-                    width={180}
-                    height={180}
-                    priority
-                />
-                <p
-                    className="text-center"
-                    style={{
-                        color: "var(--color-dark-gray)",
-                        fontSize: "var(--text-sub)",
-                        fontWeight: "var(--weight-semibold)",
-                    }}
-                >
-                    카카오와 안전하게 연결 중이에요
-                </p>
+
+            <Image
+                src="/images/cabit_hello.png"
+                alt="카카오 로그인 중인 OPU 캐릭터"
+                width={180}
+                height={180}
+                priority
+            />
+            <p
+                className="text-center"
+                style={{
+                    color: "var(--color-dark-gray)",
+                    fontSize: "var(--text-sub)",
+                    fontWeight: "var(--weight-semibold)",
+                }}
+            >
+                카카오와 안전하게 연결 중이에요
+            </p>
+            <div className="flex flex-col gap-3 items-center justify-center">
                 <p
                     className="text-center"
                     style={{
@@ -33,13 +34,13 @@ export default function KakaoLoginLoading() {
                 >
                     잠시만 기다려 주세요
                 </p>
-            </div>
 
-            {/* 스피너 */}
-            <div className="flex gap-2">
-                <span className="dot bg-[var(--color-opu-pink)]"></span>
-                <span className="dot bg-[var(--color-opu-pink)]"></span>
-                <span className="dot bg-[var(--color-opu-pink)]"></span>
+                {/* 스피너 */}
+                <div className="flex gap-2">
+                    <span className="dot bg-[var(--color-opu-pink)]"></span>
+                    <span className="dot bg-[var(--color-opu-pink)]"></span>
+                    <span className="dot bg-[var(--color-opu-pink)]"></span>
+                </div>
             </div>
         </section>
     );
