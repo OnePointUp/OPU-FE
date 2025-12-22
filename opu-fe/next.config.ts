2d8251923b22031758959848d5791aff4476af20
lddocy <jkcy911219@naver.com>
2025-12-22 22:37:42 +0900

    ➕ chore: 프록시 리라이트를 신규 API 도메인으로 업데이트

--- opu-fe/next.config.ts
+++ opu-fe/next.config.ts
@@ -15,9 +15,8 @@ const nextConfig: NextConfig = {
     async rewrites() {
         return [
             {
-                source: "/api/v1/:path*",
-                destination:
-                    "http://ec2-13-209-4-254.ap-northeast-2.compute.amazonaws.com:8080/api/v1/:path*",
+                source: "/api/(.*)",
+                destination: "https://api.onepointup.site/$1",
             },
         ];
     },
